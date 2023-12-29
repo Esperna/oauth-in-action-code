44d6150cb2bfa377e8f120633d0644846f9c2259
Esperna <esper0328@gmail.com>
2023-12-29 09:10:40 +0900

    modified Content-Type from text/html to application/json for avoiding XSS

--- exercises/ch-8-ex-1/protectedResource.js
+++ exercises/ch-8-ex-1/protectedResource.js
@@ -85,22 +85,25 @@ var requireAccessToken = function (req, res, next) {
 
 app.get("/helloWorld", getAccessToken, function (req, res) {
     if (req.access_token) {
+        let resource = {
+            greeting: "",
+        };
         if (req.query.language == "en") {
-            res.send("Hello World");
+            resource.greeting = "Hello World";
         } else if (req.query.language == "de") {
-            res.send("Hallo Welt");
+            resource.greeting = "Hallo Welt";
         } else if (req.query.language == "it") {
-            res.send("Ciao Mondo");
+            resource.greeting = "Ciao Mondo";
         } else if (req.query.language == "fr") {
-            res.send("Bonjour monde");
+            resource.greeting = "Bonjour monde";
         } else if (req.query.language == "es") {
-            res.send("Hola mundo");
+            resource.greeting = "Hola mundo";
         } else {
-            res.send(
+            resource.greeting =
                 "Error, invalid language: " +
-                    querystring.escape(req.query.language)
-            );
+                querystring.escape(req.query.language);
         }
+        res.json(resource);
     }
 });
 
